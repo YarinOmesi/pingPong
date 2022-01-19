67c65fb009c996219b7f601a15feca9d8b5fdd56
Yarin Omesi <yarinomesi@gmail.com>
2022-01-19 17:52:35 +0200

    Added ListenerFactory

---
+++ pingPong/pingPong.CoreAbstractions/Listener/IClientListenerFactory.cs
@@ -0,0 +1,9 @@
+﻿using pingPong.CoreAbstractions.Client;
+
+namespace pingPong.CoreAbstractions.Listener
+{
+    public interface IClientListenerFactory
+    {
+        public IClientListener Create(int port);
+    }
+}
