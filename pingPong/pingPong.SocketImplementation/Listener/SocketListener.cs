2f7ba10d9dafa89a9082b453592da335b8228b31
Yarin Omesi <yarinomesi@gmail.com>
2022-01-19 17:31:55 +0200

    Wrote Handling client part

--- pingPong/pingPong.SocketImplementation/Listener/SocketListener.cs
+++ pingPong/pingPong.SocketImplementation/Listener/SocketListener.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System;
 using pingPong.SocketImplementation.Client;
+using System.Threading.Tasks;
 
 namespace pingPong.SocketImplementation.Listener
 {
@@ -22,17 +23,33 @@ namespace pingPong.SocketImplementation.Listener
         public void StartListening()
         {
             var server = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            //server.Bind(new IPEndPoint(new IPAddress(), _port));
-            
-            void HandleClient(Socket client)
+            //server.Bind
+
+            try
             {
-                var buffer = new byte[1024];
-                int bytesRead;
-                var handler = _clientHandlerFactory.Create(new SocketWriter(client));
-                while((bytesRead = client.Receive(buffer)) > 0){
-                    var span = new Span<byte>(buffer, 0, bytesRead);
-                    // call handle.HandleClient
+                while (true)
+                {
+                    var client = server.Accept();
+                    Task.Run(() =>
+                    {
+                        HandleClient(client);
+                    });
                 }
+            }catch(Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+        }
+        private void HandleClient(Socket client)
+        {
+            var buffer = new byte[1024];
+            int bytesRead;
+            var handler = _clientHandlerFactory.Create(new SocketWriter(client));
+            while ((bytesRead = client.Receive(buffer)) > 0)
+            {
+                var span = new Span<byte>(buffer, 0, bytesRead);
+                handler.HandleClient(span.ToArray());
             }
         }
     }
