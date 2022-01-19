c2da20d5ccfe8722ca9022784259897fc9ddb1e8
Yarin Omesi <yarinomesi@gmail.com>
2022-01-19 17:18:02 +0200

    Added SocketListener Factory

---
+++ pingPong/pingPong.SocketImplementation/Listener/SocketListenerFactory.cs
@@ -0,0 +1,12 @@
+﻿using pingPong.CoreAbstractions.Listener;
+
+namespace pingPong.SocketImplementation.Listener
+{
+    public class SocketListenerFactory : IClientListenerFactory
+    {
+        public IClientListener Create(int port)
+        {
+            return new SocketListener(port,);
+        }
+    }
+}
