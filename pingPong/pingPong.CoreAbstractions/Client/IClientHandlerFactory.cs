d7f05e60e0e4246c80e93cd6e923a091d60c6b25
Yarin Omesi <yarinomesi@gmail.com>
2022-01-19 16:14:12 +0200

    Added IClientHandlerFactory

---
+++ pingPong/pingPong.CoreAbstractions/Client/IClientHandlerFactory.cs
@@ -0,0 +1,8 @@
+﻿
+namespace pingPong.CoreAbstractions.Client
+{
+    internal interface IClientHandlerFactory<TIn,TOut>
+    {
+        IClientHandler<TIn> Create(IClientWriter<TOut> writer);
+    }
+}
