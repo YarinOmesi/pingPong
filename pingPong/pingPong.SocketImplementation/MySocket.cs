b935f838866d94723f3097f911c159b65c1ed271
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 09:05:00 +0200

    Implemented Socket and server with Socket

---
+++ pingPong/pingPong.SocketImplementation/MySocket.cs
@@ -0,0 +1,25 @@
+﻿using pingPong.SocketsAbstractions;
+using System.Net.Sockets;
+
+namespace pingPong.SocketImplementation
+{
+    public class MySocket:ISocket
+    {
+        private readonly Socket _client;
+
+        public MySocket(Socket client)
+        {
+            _client = client;
+        }
+
+        public int Receive(byte[] buffer)
+        {
+            return _client.Receive(buffer);
+        }
+
+        public void Send(byte[] data)
+        {
+            _client.Send(data);
+        }
+    }
+}
---
+++ pingPong/pingPong.SocketImplementation/SocketServer.cs
@@ -0,0 +1,36 @@
+﻿using pingPong.SocketsAbstractions;
+using System.Net;
+using System.Net.Sockets;
+
+namespace pingPong.SocketImplementation
+{
+    public class SocketServer : IServerSocket
+    {
+        private readonly Socket _server;
+        private readonly IPAddress _addr;
+        private readonly int _port;
+
+        public SocketServer(IPAddress addr, int port)
+        {
+            _addr = addr;
+            _port = port;
+        }
+
+        public ISocket AcceptClient()
+        {
+            var client = _server.Accept();
+            return new MySocket(client);
+        }
+
+        public void Start()
+        {
+            _server.Bind(new IPEndPoint(_addr, _port));
+            _server.Listen();
+        }
+
+        public void Stop()
+        {
+            _server.Close();
+        }
+    }
+}
