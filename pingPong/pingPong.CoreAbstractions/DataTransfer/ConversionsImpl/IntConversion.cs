ea9b77a1a49220db8deef0040dbe17dfa0fbb45f
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 21:37:58 +0200

    Added Int & String Convetions, Deleted All Implementations Of IObjectSocket

---
+++ pingPong/pingPong.CoreAbstractions/DataTransfer/ConversionsImpl/IntConversion.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace pingPong.CoreAbstractions.DataTransfer.ConversionsImpl
+{
+    public class IntConversion:IEncoder,IDecoder
+    {
+        public byte[] Encode(object value)
+        {
+            return BitConverter.GetBytes((int) value);
+        }
+
+        public object Decode(byte[] bytes)
+        {
+            return BitConverter.ToInt32(bytes);
+        }
+    }
+}
---
+++ pingPong/pingPong.CoreAbstractions/DataTransfer/ConversionsImpl/StringConversion.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Text;
+
+namespace pingPong.CoreAbstractions.DataTransfer.ConversionsImpl
+{
+    public class StringConversion:IEncoder,IDecoder
+    {
+
+        private readonly Encoding _encoding;
+
+        public StringConversion(Encoding encoding)
+        {
+            _encoding = encoding;
+        }
+
+        public byte[] Encode(object value)
+        {
+            return _encoding.GetBytes((string)value);
+        }
+
+        public object Decode(byte[] bytes)
+        {
+            return _encoding.GetString(bytes);
+        }
+    }
+}
