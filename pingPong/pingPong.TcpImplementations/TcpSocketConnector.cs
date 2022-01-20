a42683d1f5a8c822321859354a98b56754ab981a
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 09:22:17 +0200

    Implemeted TcpSocketConnector using TcpClient

---
+++ pingPong/pingPong.TcpImplementations/TcpSocketConnector.cs
@@ -0,0 +1,16 @@
+﻿using pingPong.SocketsAbstractions;
+using System.Net;
+using System.Net.Sockets;
+
+namespace pingPong.TcpImplementations
+{
+    public class TcpSocketConnector : ISocketConnector
+    {
+        public ISocket Connect(IPAddress addr, int port)
+        {
+            var client = new TcpClient();
+            client.Connect(new IPEndPoint(addr, port));
+            return new TcpSocket(client);
+        }
+    }
+}
