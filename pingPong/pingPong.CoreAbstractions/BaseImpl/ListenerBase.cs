d8b55c2f964648dba289af136af6bbb462948a4c
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 09:41:01 +0200

    Added Base Implementation of listener

---
+++ pingPong/pingPong.CoreAbstractions/BaseImpl/ListenerBase.cs
@@ -0,0 +1,45 @@
+﻿using pingPong.CoreAbstractions.Listener;
+using pingPong.CoreAbstractions.Client;
+using pingPong.SocketsAbstractions;
+using System;
+using System.Net;
+
+namespace pingPong.CoreAbstractions.BaseImpl
+{
+    public class ListenerBase : IListener
+    {
+        private readonly int _port;
+        private readonly IClientHandlerFactory<ISocket, byte[]> _clientHandlerFactory;
+        private readonly IServerSocketFactory _serverSocketFactory;
+
+        public ListenerBase(int port, IClientHandlerFactory<ISocket, byte[]> clientHandlerFactory, IServerSocketFactory serverSocketFactory)
+        {
+            _port = port;
+            _clientHandlerFactory = clientHandlerFactory;
+            _serverSocketFactory = serverSocketFactory;
+        }
+
+        public void StartListening()
+        {
+            var server = _serverSocketFactory.Create(IPAddress.Any,_port);
+            try
+            {
+                server.Start();
+                while (true)
+                {
+                    var client=server.AcceptClient();
+                    var handler = _clientHandlerFactory.Create(new SocketWriter(client));
+                    handler.HandleClient(client);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error {e.Message}");
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+    }
+}
---
+++ pingPong/pingPong.CoreAbstractions/BaseImpl/SocketWriter.cs
@@ -0,0 +1,20 @@
+﻿using pingPong.CoreAbstractions.Client;
+using pingPong.SocketsAbstractions;
+
+namespace pingPong.CoreAbstractions.BaseImpl
+{
+    public class SocketWriter : IClientWriter<byte[]>
+    {
+        private readonly ISocket _client;
+
+        public SocketWriter(ISocket client)
+        {
+            _client = client;
+        }
+
+        public void Write(byte[] value)
+        {
+            _client.Send(value);
+        }
+    }
+}
