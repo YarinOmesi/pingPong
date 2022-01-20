e03202149fb50371a98bd6c6409cb4aea577ad06
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 09:56:15 +0200

    Added ServerSocket factory

---
+++ pingPong/pingPong.SocketImplementation/SocketServerFactory.cs
@@ -0,0 +1,13 @@
+﻿using pingPong.SocketsAbstractions;
+using System.Net;
+
+namespace pingPong.SocketImplementation
+{
+    public class SocketServerFactory : IServerSocketFactory
+    {
+        public IServerSocket Create(IPAddress address, int port)
+        {
+            return new SocketServer(address, port);
+        }
+    }
+}
---
+++ pingPong/pingPong.TcpImplementations/TcpServerFactory.cs
@@ -0,0 +1,13 @@
+﻿using pingPong.SocketsAbstractions;
+using System.Net;
+
+namespace pingPong.TcpImplementations
+{
+    public class TcpServerFactory : IServerSocketFactory
+    {
+        public IServerSocket Create(IPAddress address, int port)
+        {
+            return new TcpServerSocket(address, port);
+        }
+    }
+}
