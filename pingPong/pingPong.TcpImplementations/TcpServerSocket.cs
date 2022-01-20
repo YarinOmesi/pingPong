36728a54ca9e06fe882c4ffa5c1011ffdc0e9b3d
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 09:13:56 +0200

    Added TcpServer using TcpListener

---
+++ pingPong/pingPong.TcpImplementations/TcpServerSocket.cs
@@ -0,0 +1,38 @@
+﻿
+using pingPong.SocketsAbstractions;
+using System.Net;
+using System.Net.Sockets;
+
+namespace pingPong.TcpImplementations
+{
+    public class TcpServerSocket : IServerSocket
+    {
+        private readonly TcpListener _server;
+        private readonly IPAddress _addr;
+        private readonly int _port;
+
+        public TcpServerSocket(IPAddress addr, int port)
+        {
+            _addr = addr;
+            _port = port;
+            _server = new TcpListener(addr, port);
+        }
+
+        public ISocket AcceptClient()
+        {
+            var client = _server.AcceptTcpClient();
+
+            return new TcpSocket(client);
+        }
+
+        public void Start()
+        {
+            _server.Start();
+        }
+
+        public void Stop()
+        {
+            _server.Stop();
+        }
+    }
+}
