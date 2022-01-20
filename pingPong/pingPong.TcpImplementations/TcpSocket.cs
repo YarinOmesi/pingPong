5c683e208a1515831f1c837ac8d9efc39cbe112f
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 09:09:44 +0200

    Implemeted client socket using tcpClient

---
+++ pingPong/pingPong.TcpImplementations/TcpSocket.cs
@@ -0,0 +1,25 @@
+﻿using pingPong.SocketsAbstractions;
+using System.Net.Sockets;
+
+namespace pingPong.TcpImplementations
+{
+    public class TcpSocket : ISocket
+    {
+        private readonly TcpClient _client;
+
+        public TcpSocket(TcpClient client)
+        {
+            _client = client;
+        }
+
+        public int Receive(byte[] buffer)
+        {
+            return _client.GetStream().Read(buffer, 0, buffer.Length);
+        }
+
+        public void Send(byte[] data)
+        {
+            _client.GetStream().Write(data, 0, data.Length);
+        }
+    }
+}
