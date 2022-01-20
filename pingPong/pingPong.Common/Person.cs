af9269cbc988d22168611eee63762529fcf7e7ab
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 11:44:56 +0200

    Added Common project

---
+++ pingPong/pingPong.Common/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pingPong.Common
+{
+    public class Person
+    {
+        private string _name;
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name} is {_age} years old";
+        }
+    }
+}
