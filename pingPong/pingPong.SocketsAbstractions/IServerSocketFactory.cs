46e159004c9123cf030e299031cfe63c85b0afa4
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 09:40:17 +0200

    Added IServerSocketFactory

---
+++ pingPong/pingPong.SocketsAbstractions/IServerSocketFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace pingPong.SocketsAbstractions
+{
+    public interface IServerSocketFactory
+    {
+        public IServerSocket Create(IPAddress address, int port);/
+    }
+}
