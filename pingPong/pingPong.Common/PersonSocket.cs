d538445cbb98ad043b9a441bcaec714879b20eac
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 11:49:32 +0200

    Added Refrence to Common& Created PersonSocket

---
+++ pingPong/pingPong.Common/PersonSocket.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text.Json;
+using pingPong.SocketsAbstractions;
+
+namespace pingPong.Common
+{
+    public class PersonSocket:IObjectSocket<Person>
+    {
+        private readonly IObjectSocket<string> _stringSocket;
+
+        public PersonSocket(IObjectSocket<string> stringSocket)
+        {
+            _stringSocket = stringSocket;
+        }
+
+        public Person Receive()
+        {
+            var json = _stringSocket.Receive();
+
+            return JsonSerializer.Deserialize<Person>(json);
+        }
+
+        public void Send(Person value)
+        {
+            var json = JsonSerializer.Serialize(value);
+            _stringSocket.Send(json);
+        }
+    }
+}
