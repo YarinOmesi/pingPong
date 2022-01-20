58fb714ea3a9663e43dc226fb8a77a7092f35283
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 21:59:13 +0200

    Implenented the bootstrap in the Program

--- pingPong/pingPong.ClientImplementation/Program.cs
+++ pingPong/pingPong.ClientImplementation/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
-using pingPong.Common;
+using System.Runtime.Serialization.Formatters.Binary;
+using pingPong.CoreAbstractions.DataTransfer;
+using pingPong.CoreAbstractions.DataTransfer.ConversionsImpl;
+using pingPong.CoreAbstractions.Protocol;
 using pingPong.SocketImplementation;
 
 namespace pingPong.ClientImplementation
@@ -11,8 +15,19 @@ namespace pingPong.ClientImplementation
         {
             var socketConnector = new SocketConnector();
             var socket = socketConnector.Connect(IPAddress.Parse("127.0.0.1"),int.Parse(args[0]));
-            var personSocket = new PersonSocket(socket);
-            var client = new PersonClient(personSocket);
+
+            var personConversion = new ObjectConversionBase(new BinaryFormatter());
+            var encoders = new Dictionary<Type, IEncoder>
+            {
+                {typeof(Person),personConversion}
+            };
+            var decoders = new Dictionary<Type, IDecoder>
+            {
+                {typeof(Person),personConversion}
+            };
+            var conversion = new Conversions(encoders, decoders);
+            var protocol = new ProtocolBase(conversion,socket);
+            var client = new PersonClient(protocol);
             client.Run();
         }
     }
