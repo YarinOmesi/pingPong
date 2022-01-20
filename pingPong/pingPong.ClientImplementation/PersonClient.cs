d4eae02a40810ab2f161c70f278b0a40802b2f83
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 17:26:10 +0200

    Added PersonClient implementation

---
+++ pingPong/pingPong.ClientImplementation/PersonClient.cs
@@ -0,0 +1,25 @@
+﻿
+using pingPong.Common;
+using pingPong.CoreAbstractions.Client;
+using pingPong.SocketsAbstractions;
+
+namespace pingPong.ClientImplementation
+{
+    internal class PersonClient:ClientBase<Person>
+    {
+        public PersonClient(IObjectSocket<Person> socket)
+            : base(socket, "PersonClient")
+        {
+        }
+
+        public override void Run()
+        {
+            Person? person;
+            while ((person = _socket.Receive())!=null)
+            {
+                _logger.Debug($"Recved {person}");
+                _socket.Send(person);
+            }
+        }
+    }
+}
