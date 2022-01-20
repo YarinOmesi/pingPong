e32d7a8a4cdc2e5e4e3914ae128f2756312b555e
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 09:20:04 +0200

    Implemented SocketConnector using Socket

---
+++ pingPong/pingPong.SocketImplementation/SocketConnector.cs
@@ -0,0 +1,16 @@
+﻿using pingPong.SocketsAbstractions;
+using System.Net;
+using System.Net.Sockets;
+
+namespace pingPong.SocketImplementation
+{
+    public class SocketConnector : ISocketConnector
+    {
+        public ISocket Connect(IPAddress addr, int port)
+        {
+            var client = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(new IPEndPoint(addr, port));
+            return new MySocket(client);
+        }
+    }
+}
