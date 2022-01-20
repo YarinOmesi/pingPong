990a86f48ebd3a69403eb4e20dcb2b4700dd2272
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 21:43:10 +0200

    Added ObjectConversionBase

---
+++ pingPong/pingPong.CoreAbstractions/DataTransfer/ConversionsImpl/ObjectConversionBase.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+
+namespace pingPong.CoreAbstractions.DataTransfer.ConversionsImpl
+{
+    public class ObjectConversionBase:IEncoder,IDecoder
+    {
+        private readonly IFormatter _formatter;
+
+        public ObjectConversionBase(IFormatter formatter)
+        {
+            _formatter = formatter;
+        }
+
+        public byte[] Encode(object value)
+        {
+            var memoryBuffer = new MemoryStream();
+            _formatter.Serialize(memoryBuffer,value);
+            return memoryBuffer.ToArray();
+        }
+
+        public object Decode(byte[] bytes)
+        {
+            return _formatter.Deserialize(new MemoryStream(bytes));
+        }
+    }
+}
