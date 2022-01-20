3a883256aa3472ee6cce70767c404e5bed1db6aa
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 17:00:46 +0200

    Moved And Renamed to ClientBase

---
+++ pingPong/pingPong.CoreAbstractions/Client/ClientBase.cs
@@ -0,0 +1,20 @@
+﻿using pingPong.Logger;
+using pingPong.SocketsAbstractions;
+
+namespace pingPong.CoreAbstractions.Client
+{
+    public abstract class ClientBase<T>
+    {
+        protected readonly ILogger _logger;
+        protected readonly IObjectSocket<T> _socket;
+
+        protected ClientBase( IObjectSocket<T> socket)
+        {
+            _socket = socket;
+            _logger = new Logger.Logger().GetLogger("Client");
+        }
+
+        public abstract void Run();
+
+    }
+}
