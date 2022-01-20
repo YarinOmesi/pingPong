504c581cc6cdab3826943849afacb7c1b421c341
Yarin Omesi <yarinomesi@gmail.com>
2022-01-20 11:12:28 +0200

    Added IObjectSocket

---
+++ pingPong/pingPong.SocketsAbstractions/IObjectSocket.cs
@@ -0,0 +1,10 @@
+﻿
+namespace pingPong.SocketsAbstractions
+{
+    public interface IObjectSocket<T>
+    {
+        public T Receive(ISocket socket);
+
+        public void Send(ISocket socket, T value);
+    }
+}
